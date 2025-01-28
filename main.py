e155ff37d59e98d0be1cb2cd99bf94c631414fc2
Jem <jemstobart@hotmail.com>
2025-01-28 13:58:22 +0000

    Add sqs message sending

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template, flash, request
+import json
+import os
+
+import boto3
+from flask import Flask, render_template, flash, request, redirect, url_for
+from dotenv import load_dotenv, dotenv_values
+# loading variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
+sqs = boto3.client('sqs', region_name=os.getenv('AWS_REGION'))
 
 @app.route('/', methods=["GET", "POST"])
 def priority_form():
@@ -15,7 +23,25 @@ def priority_form():
         elif not description:
             flash('Description is required!')
         else:
-            priority_message = {'title': title, 'description': description, 'priority': priority}
+            priority_message = {'title': title, 'description': description}
+            if priority == 1:
+                response = sqs.send_message(
+                    QueueUrl=os.getenv('P1_QUEUE'),
+                    MessageBody=priority_message
+                )
+            elif priority == 2:
+                response = sqs.send_message(
+                    QueueUrl=os.getenv('P2_QUEUE'),
+                    MessageBody=priority_message
+                )
+            else:
+                response = sqs.send_message(
+                    QueueUrl=os.getenv('P3_QUEUE'),
+                    MessageBody=priority_message
+                )
+            print(f'Message sent: {response["MessageId"]}')
+            return redirect(url_for('/'))
+
 
 
     return render_template("priority.html", priorities=priorities)
