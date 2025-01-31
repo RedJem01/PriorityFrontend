4e6d67544d498af1f69e51d51318ad42bde8a0f4
Jem <jemstobart@hotmail.com>
2025-01-31 13:55:12 +0000

    Add loggers

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import os
 
 import boto3
@@ -17,47 +18,60 @@ P3_QUEUE = os.getenv('P3_QUEUE')
 app = Flask(__name__)
 app.secret_key = 'the random string'
 
+logger = logging.getLogger(__name__)
+
 #Make SQS client
-sqs = boto3.client('sqs', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY)
+sqs = boto3.client(
+    'sqs',
+    region_name=AWS_REGION,
+    aws_access_key_id=ACCESS_KEY,
+    aws_secret_access_key=SECRET_ACCESS_KEY
+)
 
 @app.route('/form', methods=["GET", "POST"])
 def priority_form():
     #For drop down menu
     priorities = ['Low', 'Medium', 'High']
     if request.method == "POST":
+
         #Get inputs
         title = request.form["title"]
         description = request.form["description"]
-        priority_match = {"High": 1, "Medium": 2, "Low": 3}
         #Get priority number
-        priority = priority_match[request.form["priority"]]
+        priority = request.form["priority"]
+
         #Check title and description were entered
         if not title:
             flash('Title is required!')
+            logger.warning("Title not on POST request")
             return redirect(url_for('priority_form'))
         elif not description:
             flash('Description is required!')
+            logger.warning("Title not on POST request")
             return redirect(url_for('priority_form'))
+
         else:
             #Make SQS message body
             priority_message = json.dumps({'title': title, 'description': description})
             #Send message to each separate priority queue
-            if priority == 1:
+            if priority == "High":
+                logger.info("SQS message sent to priority queue 1 with message" + priority_message)
                 response = sqs.send_message(
                     QueueUrl=P1_QUEUE,
                     MessageBody=priority_message
                 )
-            elif priority == 2:
+            elif priority == "Medium":
+                logger.info("SQS message sent to priority queue 2 with message" + priority_message)
                 response = sqs.send_message(
                     QueueUrl=P2_QUEUE,
                     MessageBody=priority_message
                 )
             else:
+                logger.info("SQS message sent to priority queue 3 with message" + priority_message)
                 response = sqs.send_message(
                     QueueUrl=P3_QUEUE,
                     MessageBody=priority_message
                 )
-            print(f'Message sent: {response["MessageId"]}')
             return redirect(url_for('priority_form'))
 
 
@@ -66,7 +80,7 @@ def priority_form():
 #Helth check for api
 @app.route('/health', methods=['GET'])
 def health_check():
-    return 'Service is all good', 200
+    return 'OK', 200
 
 if __name__ == '__main__':
     app.run()
